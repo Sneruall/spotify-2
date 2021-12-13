fc1ce12c2a3ddc12c5d60f764f89c01c55000606
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-12-13 09:34:55 +0100

    project init and start

---
+++ components/Sidebar.js
@@ -0,0 +1,69 @@
+import {
+  HomeIcon,
+  SearchIcon,
+  LibraryIcon,
+  PlusCircleIcon,
+  RssIcon,
+  HeartIcon,
+} from "@heroicons/react/outline";
+
+function Sidebar() {
+  return (
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+      <div className="flex flex-col space-y-4">
+        <button>
+          <span className="flex items-center space-x-2 hover:text-white">
+            <HomeIcon className="h-5 w-5" />
+            <p>Home</p>
+          </span>
+        </button>
+        <button>
+          <span className="flex items-center space-x-2 hover:text-white">
+            <SearchIcon className="h-5 w-5" />
+            <p>Search</p>
+          </span>
+        </button>
+        <button>
+          <span className="flex items-center space-x-2 hover:text-white">
+            <LibraryIcon className="h-5 w-5" />
+            <p>Your Library</p>
+          </span>
+        </button>
+        <hr className="border-t-[0.1px] border-gray-900" />
+        <button>
+          <span className="flex items-center space-x-2 hover:text-white">
+            <PlusCircleIcon className="h-5 w-5" />
+            <p>Create Playlist</p>
+          </span>
+        </button>
+        <button>
+          <span className="flex items-center space-x-2 hover:text-white">
+            <HeartIcon className="h-5 w-5" />
+            <p>Liked Songs</p>
+          </span>
+        </button>
+        <button>
+          <span className="flex items-center space-x-2 hover:text-white">
+            <RssIcon className="h-5 w-5" />
+            <p>Your episodes</p>
+          </span>
+        </button>
+        <hr className="border-t-[0.1px] border-gray-900" />
+        {/* Playlists */}
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;

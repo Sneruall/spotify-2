27b941c5c78869fabaef3082c1dacbbbbe54d7bf
Laurens <36268730+Sneruall@users.noreply.github.com>
2021-12-20 19:04:31 +0100

    customizing the sign in page

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -6,11 +6,20 @@ import {
   RssIcon,
   HeartIcon,
 } from "@heroicons/react/outline";
+import { signOut, useSession } from "next-auth/react";
 
 function Sidebar() {
+  const { data: session, status } = useSession();
+  console.log(session);
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
       <div className="flex flex-col space-y-4">
+        <button>
+          <span className="flex items-center space-x-2 hover:text-white">
+            <HomeIcon className="h-5 w-5" onClick={() => signOut()} />
+            <p>Logout</p>
+          </span>
+        </button>
         <button>
           <span className="flex items-center space-x-2 hover:text-white">
             <HomeIcon className="h-5 w-5" />
